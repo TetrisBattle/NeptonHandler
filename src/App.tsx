46403fb48e95584a6361dee4d1b94900b22c5b92
Thien Nguyen <thien.nguyen@eskosystems.fi>
2026-06-29 16:27:37 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function App() {
+	return (
+		<Box sx={{ p: 2, minWidth: 320 }}>
+			<Typography variant="h6" gutterBottom>
+				Nepton Handler
+			</Typography>
+		</Box>
+	);
+}
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const theme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+);
